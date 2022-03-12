2d4650345b21b2fe5d11efdb992b0e3b684d1dbf
MzPoju <adepojuoopeye@gmail.com>
2022-03-12 14:44:13 -0800

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
  * main - prints the alphabet in lowercase, and then in uppercase,
  * followed by a new line
- * Return: ways 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
-	for (ch ='A'; ch <= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
