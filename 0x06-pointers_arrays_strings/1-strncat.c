4afc35e6da3c0b3405d4551263ffdc76981d0127
MzPoju <adepojuoopeye@gmail.com>
2022-03-24 16:20:04 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char destination[] = "Hello ";
+	char source[] = "World!\n";
+
+	printf ( "\nsource string = %s", source );
+	printf ( "\ndestination string = %s", destination );
+
+	strncat ( destination, source, 15 );
+
+	printf ( "\ndestination string after strncat() = %s", destination);
+
+	return (0);
+}
