23dccd9e81d1eda4a8e8092e010818972b8dd835
MzPoju <adepojuoopeye@gmail.com>
2022-03-24 16:56:48 -0700

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 #include <string.h>
 
 int main(void)
@@ -6,12 +6,12 @@ int main(void)
 	char destination[] = "Hello ";
 	char source[] = "World!\n";
 
-	printf ( "\nsource string = %s", source );
-	printf ( "\ndestination string = %s", destination );
+	printf("\nsource string = %s", source);
+	printf("\ndestination string = %s", destination );
 
-	strncat ( destination, source, 15 );
+	strncat(destination, source, 15);
 
-	printf ( "\ndestination string after strncat() = %s", destination);
+	printf("\ndestination string after strncat() = %s", destination);
 
 	return (0);
 }
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+
+
+#include <string.h>
+
+int main(void)
+{
+	char str1[100],str2[50];
+
+	printf("Enter string str1\n");
+	gets(str1);
+
+	strcpy(str2,str1);
+	printf("Copied String(str2) is %s", str 2);
+
+}
+	
