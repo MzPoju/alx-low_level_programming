2484938f771b72e6507faf75f8c0430170c02d82
MzPoju <adepojuoopeye@gmail.com>
2022-03-28 16:36:35 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ *
+ * @s: segment to return bytes from
+ * @accept: the bytes to include
+ *
+ * Return: the number of bytes in the initial segment of @s which consist 
