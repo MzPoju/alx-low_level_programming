ce65fdab3a37165cf6b59cc9ae7a6c743eff68cf
MzPoju <adepojuoopeye@gmail.com>
2022-12-09 02:27:57 -0800

    compiled language

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
-
-
 /**
- *
- *  * main - print
- *
- *   * Return: Always 1 (successful)
- *
- *    */
+ *main - Entry point
+ *Return: Always 1
+ */
 
 int main(void)
-
 {
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
