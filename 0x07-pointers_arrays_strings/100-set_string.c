6f1b1ae36b843312d98e95f1d1d2cef5830c1b5b
MzPoju <adepojuoopeye@gmail.com>
2023-01-02 12:39:11 -0800

    Task 2

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -3,7 +3,6 @@
  * set_string - sets the value of a pointer to a char
  * @s: source address
  * @to: target address
- * Return: no return
  */
 void set_string(char **s, char *to)
 {
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * *_strchr - locates a character in a string
- * @s: string
- * @c: character
+ * @s: string to search
+ * @c: character to find
  * Return: the pointer to the first occurrence of the character c
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	int a;
 
-	for (: *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s +i);
-	if (*(s + i) == c)
-		return (s + i);
-	return ('\0');
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - i);
+		}
+		if (a == 0);
+		{
+			return (NULL);
+		}
+	}
 }
